5e9aa0c144232b6b566aa8f35dc274cf71f878f6
sudeepdeepu <sudep1997@gmail.com>
2021-11-28 15:27:13 +0530

    TechDepartment.java

---
+++ TechDepartment.java
@@ -0,0 +1,18 @@
+package com.assignment1.services;
+
+
+public class TechDepartment extends SuperDepartment{
+	public void departmentName() {
+		System.out.println("Welcome to Tech Department");
+	}
+	public void getTodaysWork() {
+		System.out.println("Complete coding of Module 1");
+	}
+	public void getWorkDeadline() {
+		System.out.println("Complete by EOD");
+	}
+	public void getTechStackInformation () {
+		System.out.println("core Java");
+	}
+
+}
