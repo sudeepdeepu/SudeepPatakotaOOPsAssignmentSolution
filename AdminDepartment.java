09c1aaa65b7fa5240c631f8561a69a0ae12c06d2
sudeepdeepu <sudep1997@gmail.com>
2021-11-28 15:26:13 +0530

    AdminDepartment.java

---
+++ AdminDepartment.java
@@ -0,0 +1,13 @@
+package com.assignment1.services;
+
+public class AdminDepartment extends SuperDepartment{
+	public void departmentName() {
+		System.out.println("Welcome to Admin Department");
+	}
+	public void getTodaysWork() {
+		System.out.println("Complete your documents submission");
+	}
+	public void getWorkDeadline() {
+		System.out.println("Complete by EOD");
+	}
+}
