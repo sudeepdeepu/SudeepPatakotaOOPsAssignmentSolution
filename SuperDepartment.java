b4de8861d0108de04634bdcdce1fca0f6151c0b8
sudeepdeepu <sudep1997@gmail.com>
2021-11-28 15:25:39 +0530

    SuperDepartment.java

---
+++ SuperDepartment.java
@@ -0,0 +1,17 @@
+package com.assignment1.services;
+
+public class SuperDepartment {
+	public void departmentName() {
+		System.out.println("Welcome to Super Department");
+	}
+	public void getTodaysWork() {
+		System.out.println("No Work as of now");
+	}
+	public void getWorkDeadline() {
+		System.out.println("Nil");
+	}
+	public void isTodayAHoliday() {
+		System.out.println("Today is not a Holiday\n");
+	}
+
+}
